3c0d6e7ab557853558b54fdc42f95b9336d2567a
徐顺望 <594412874@qq.com>
2021-03-23 09:58:16 +0800

    2021-3-23版本1.4.20

--- pages/member/memberFollow/memberFollow.js
+++ pages/member/memberFollow/memberFollow.js
@@ -1,6 +1,5 @@
 Page({
   data: {
-    show:null  //1 安卓机 2 苹果机
   },
   onLoad: function (options) {
 
@@ -9,20 +8,6 @@ Page({
 
   },
   onShow: function () {
-    let that = this
-    wx.getSystemInfo({
-      success (res) {
-        let showNew = null
-        let str = res.model
-        if(str.indexOf("iPhone") !== -1){
-          showNew = 2
-        }else{
-          showNew = 1
-        }
-        that.setData({
-          show:showNew,
-        })
-      }
-    })
+
   },
 })
